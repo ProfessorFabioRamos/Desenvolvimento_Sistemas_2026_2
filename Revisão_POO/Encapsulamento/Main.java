a0504e978c972c77acf16178355739e2bb07acfb
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-13 10:46:50 -0300

    Create Main.java

---
+++ Revisão_POO/Encapsulamento/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Aluno aluno_1 = new Aluno("Nayara  Santos",
+                23,
+                "Publicidade");
+
+        aluno_1.setNome("Naiara  Santos");
+        System.out.println(aluno_1.getNome());
+    }
+}
