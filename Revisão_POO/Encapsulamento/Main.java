4f320e6b54edc4e275f473ba2cc918e61d82d8bf
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-20 09:03:57 -0300

    Update Main.java

--- Revisão_POO/Encapsulamento/Main.java
+++ Revisão_POO/Encapsulamento/Main.java
@@ -1,10 +1,16 @@
 public class Main{
     public static void main(String[] args){
-        Aluno aluno_1 = new Aluno("Nayara  Santos",
+        Aluno aluno_1 = new Aluno("Nayara Santos",
                 23,
                 "Publicidade");
 
-        aluno_1.setNome("Naiara  Santos");
+        aluno_1.setNome("Naiara Santos");
         System.out.println(aluno_1.getNome());
+
+        aluno_1.setCurso("Engenharia da Computação");
+        System.out.println(aluno_1.getCurso());
+
+        aluno_1.setCurso(1);
+        System.out.println(aluno_1.getCurso());
     }
 }
