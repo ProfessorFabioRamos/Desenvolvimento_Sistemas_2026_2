61db5499989aedbf8abc55cf63d19d93bd8f8486
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-20 09:04:12 -0300

    Update Aluno.java

--- Revisão_POO/Encapsulamento/Aluno.java
+++ Revisão_POO/Encapsulamento/Aluno.java
@@ -15,6 +15,10 @@ public class Aluno {
     }
 
     public String getNome() {
+        return toUp(nome);
+    }
+
+    private String toUp(String strToUp){
         return nome.toUpperCase();
     }
 
@@ -22,5 +26,21 @@ public class Aluno {
         if(novoNome != null){
             nome = novoNome;
         }
+    }
+
+    public void setCurso(String novoCurso) {
+        if(novoCurso != null){
+            curso = novoCurso;
+        }
+    }
+    public void setCurso(int codigo) {
+        if (codigo == 1) curso = "Ciência da Computação";
+        else if (codigo == 2) curso = "Ciência de Dados";
+        else if (codigo == 3) curso = "ADS";
+        else System.out.println("Curso Inválido");
+    }
+
+    public String getCurso(){
+        return curso;
     }
 }
