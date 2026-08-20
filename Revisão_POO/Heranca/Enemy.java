553cc7175509ca2e2b4e22a215a13a5be428b59c
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-20 10:47:05 -0300

    Create Enemy.java

---
+++ Revisão_POO/Heranca/Enemy.java
@@ -0,0 +1,19 @@
+public class Enemy {
+    public String raca;
+    public int hp;
+    public int damage;
+    public double speed;
+
+    public Enemy(String raca, int hp, int damage, double speed) {
+        this.raca = raca;
+        this.hp = hp;
+        this.damage = damage;
+        this.speed = speed;
+    }
+    public void emitirSom(){
+        System.out.println("RARRRRRRRR");
+    }
+    public void tomarDano(int dano){
+        hp-=dano;
+    }
+}
