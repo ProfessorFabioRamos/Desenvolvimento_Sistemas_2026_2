45178318346879766407eba3bb81848ed463a617
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-08-27 08:55:14 -0300

    Update Main.java

--- Revisão_POO/Heranca/Main.java
+++ Revisão_POO/Heranca/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Orc orc_1 = new Orc(30, 10, 9,
                 "Thrall",5);
         orc_1.emitirSom();
-        orc_1.tomarDano(5);
-        System.out.println(orc_1.hp);
+        orc_1.tomarDano(20);
+        System.out.println("HP restante: "+orc_1.hp);
     }
 }
