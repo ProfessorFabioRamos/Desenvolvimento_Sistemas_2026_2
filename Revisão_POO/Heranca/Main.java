7cf63200c43f4618b92a329db91edfa2b13d257b
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-01 09:12:16 -0300

    Update Main.java

--- Revisão_POO/Heranca/Main.java
+++ Revisão_POO/Heranca/Main.java
@@ -1,14 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Enemy inimigo_1 = new Enemy("Goblin",10,
-                2,5);
-        inimigo_1.emitirSom();
-        inimigo_1.tomarDano(5);
-        System.out.println(inimigo_1.hp);
+public class Main{
+    public static void main(String[] args){
+        Enemy enemy_1 = new Enemy("Goblin",
+                10, 2,5);
+        enemy_1.emitirSom();
+        enemy_1.tomarDano(5);
+        System.out.println("HP restante: "+enemy_1.hp);
 
-        Orc orc_1 = new Orc(30, 10, 9,
+        Orc orc_1 = new Orc(30,10,9,
                 "Thrall",5);
         orc_1.emitirSom();
         orc_1.tomarDano(20);
