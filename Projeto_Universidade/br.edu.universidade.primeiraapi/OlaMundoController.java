b412b5c053aede7d7b59d82c035108e10ab74e14
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-10 10:36:35 -0300

    Create OlaMundoController.java

---
+++ Projeto_Universidade/br.edu.universidade.primeiraapi/OlaMundoController.java
@@ -0,0 +1,12 @@
+package br.edu.universidade.primeiraapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OlaMundoController {
+    @GetMapping("/ola")
+    public String dizerOla() {
+        return "Ola Mundo! Meu servidor está rodando.";
+    }
+}
