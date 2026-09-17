3cc7bebe6f2b8123a4fb12a18d550613aef3b9d7
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-17 10:24:02 -0300

    Update AlunoController.java

--- Projeto_Universidade/br.edu.universidade.primeiraapi/controller/AlunoController.java
+++ Projeto_Universidade/br.edu.universidade.primeiraapi/controller/AlunoController.java
@@ -15,10 +15,18 @@ public class AlunoController {
     private List<Aluno> alunos = new ArrayList<>();
     // Simulação do contador auto-incremental do BD
     private long proximoId = 1;
-    
+
     // Metodo GET HTTP que retorna a lista
     @GetMapping
     public List<Aluno> listarTodos() {return alunos;}
 
     // Metodo POST HTTP que cria um aluno e adiciona na lista
+    public ResponseEntity<Aluno> criarAluno(@RequestBody Aluno novoAluno) {
+        // Atribui o Id simulado e incrementa para o próximo
+        novoAluno.setId(proximoId++);
+        // Adiciona na lista
+        alunos.add(novoAluno);
+        // Retorna status 201 (Created) e o objeto criado com o novo Id
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoAluno);
+    }
 }
