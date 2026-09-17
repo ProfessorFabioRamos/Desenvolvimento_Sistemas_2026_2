4e642c5f8d3a5a6d75144f3d78f4c42e575a0714
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2026-09-17 09:20:28 -0300

    Create Aluno.java

---
+++ Projeto_Universidade/br.edu.universidade.primeiraapi/model/Aluno.java
@@ -0,0 +1,31 @@
+package br.edu.universidade.primeirapi.model;
+
+public class Aluno {
+    private Long id;
+    private String nome;
+    private String matricula;
+    private String curso;
+
+    public Aluno() {
+    }
+
+    public Aluno(Long id, String nome, String matricula, String curso) {
+        this.id = id;
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    // Getters e Setters (O Spring precisa deles para converter o JSON corretamente)
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+
+    public String getCurso() { return curso; }
+    public void setCurso(String curso) { this.curso = curso; }
+}
